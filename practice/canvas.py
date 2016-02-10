dfd327e8ea988ef48ce92c028c710e1579c06a6a
Itzik Ephraim <oranja@gmail.com>
2016-02-10 11:53:45 +0200

    Changed Canvas so it's easier to work with it from Painter

--- practice/canvas.py
+++ practice/canvas.py
@@ -6,28 +6,30 @@ FILL_CELL = '#'
 EMPTY_CELL = '.'
 
 class Canvas:
-    def __init__(self, input_file_path):
-        self.input_file_path = input_file_path
-        with open(input_file_path, 'r') as input_file:
-            self.__read_header(input_file)
-            self.__read_canvas(input_file)
+    def __init__(self, rows, columns):
+        self.rows = rows
+        self.columns = columns
+        self.canvas = [[False for column in range(self.columns)] for row in range(self.rows)]
 
-    def __read_header(self, input_file):
-        header = input_file.readline()
-        self.rows, self.columns = [int(num) for num in header.split(' ')]
-
-    def __read_canvas(self, input_file):
-        self.canvas = [None for row in range(self.rows)]
-        for row, line in enumerate(input_file):
-            self.canvas[row] = list(map(lambda cell: cell == FILL_CELL, line.strip()))
+    def fill_cell(self, row, column):
+        if (not self.is_valid_row(row)) or (not self.is_valid_column(column)):
+            return False
+        self.canvas[row][column] = True
+        return True
 
     def is_filled(self, row, column):
-        if (row < 0) or (row >= self.rows) or (column < 0) or (column >= self.columns):
+        if (not self.is_valid_row(row)) or (not self.is_valid_column(column)):
             return None
         return self.canvas[row][column]
 
+    def is_valid_row(self, row):
+        return (row >= 0) and (row < self.rows)
+
+    def is_valid_column(self, column):
+        return (column >= 0) and (column < self.columns)
+
     def __repr__(self):
-        return "A {0}X{1} canvas. Loaded from {2}".format(self.rows, self.columns, self.input_file_path)
+        return "A {0}X{1} canvas".format(self.rows, self.columns)
 
     def __str__(self):
         output = ''
@@ -40,10 +42,23 @@ class Canvas:
             output += '\n'
         return output
 
+
+def load_canvas(path):
+    """Load a Canvas from an input file"""
+    with open(path, 'r') as input_file:
+        # Read header (M N)
+        header = input_file.readline()
+        rows, columns = [int(num) for num in header.split(' ')]
+        # Read the actual painting
+        canvas = Canvas(rows, columns)
+        for row, line in enumerate(input_file):
+            canvas.canvas[row] = list(map(lambda cell: cell == FILL_CELL, line.strip()))
+        return canvas
+
 if __name__ == '__main__':
     if len(sys.argv) != 2:
         print("usage: {0} <input_file_path>".format(sys.argv[0]))
         exit(1)
-    canvas = Canvas(sys.argv[1].strip())
+    canvas = load_canvas(sys.argv[1].strip())
     print(repr(canvas))
     print(canvas)
