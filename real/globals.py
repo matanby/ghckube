e8b625982dfe52eb0846fa965e401d06757a0f8a
Matan Ben-Yosef <matan.ben.yosef@gmail.com>
2016-02-11 22:35:43 +0200

    fixes

--- real/globals.py
+++ real/globals.py
@@ -1,9 +1,20 @@
-drones = []
-products = []
-warehouses = []
-orders = []
-map_columns = -1
-map_rows = -1
-turns = -1
-drones_num = -1
-payload = -1
+class Globals(object):
+    drones = []
+    products = []
+    warehouses = []
+    orders = []
+    map_columns = -1
+    map_rows = -1
+    turns = -1
+    drones_num = -1
+    payload = -1
+
+drones = Globals.drones
+products = Globals.products
+warehouses = Globals.warehouses
+orders = Globals.orders
+map_columns = Globals.map_columns
+map_rows = Globals.map_rows
+turns = Globals.turns
+drones_num = Globals.drones_num
+payload = Globals.payload
